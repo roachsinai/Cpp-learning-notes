4987cbda503046a987ca2a015789dcbf7d1a0be5
roachsinai <710347541@qq.com>
2016-08-18 00:58:56 +0800

    update glupfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
-var gulp = require("gulp"),
-  deploy = require("gulp-gh-pages");
-
-gulp.task('deploy', function () {
-  gulp.src("_book/**/*.*")
-    .pipe(deploy({
-      remoteUrl: "https://github.com/roachsinai/Cpp-learning-notes"
-    }))
-    .on("error", function(err){
-      console.log(err)
-    })
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+ 
+gulp.task('deploy', function() {
+  return gulp.src('./_book/**/*')
+    .pipe(ghPages());
 });
